9e80cc17ce926e858450991d37953a08a8190d90
Corban <sonnemanc@gmail.com>
2022-01-08 09:36:47 -0800

    Done with Assignment

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,9 @@
 class Category < ActiveRecord::Base
   has_many :post_categories
   has_many :posts, through: :post_categories
+
+  def show
+    @category = Category.find(params[:id])
+  end
+  
 end
